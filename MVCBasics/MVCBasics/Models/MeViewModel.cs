aac5ad8430bf6cb9ade6037e823c79b281738f91
Brandon Slade <brandonyoyoslade@gmail.com>
2018-01-08 18:04:38 -0700

    feat(mvcbasics): add initial site with ugly MeController

---
+++ MVCBasics/MVCBasics/Models/MeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasics.Models
+{
+    public class MeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
